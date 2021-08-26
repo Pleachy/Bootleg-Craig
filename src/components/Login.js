fcfd56a54a485d4d88ded02e4e2d0aa8132f7e91
Pleachy <daniel.golub@gmail.com>
2021-08-25 21:36:12 -0500

    added form to login for username, password and login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
-const Login = () => {
-    return <div>Login!</div>
+const Login = (props) => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    return <div>
+        <form>
+            <div>
+                <label>Enter Username:</label>
+                <input type='text' name='password' value={username}></input>
+            </div>
+            <div>
+                <label>Enter Password:</label>
+                <input type='password' name='password' value={password}></input>
+            </div>
+            <button type='submit'>Login</button>
+        </form>
+
+
+    </div>
 }
 
 
