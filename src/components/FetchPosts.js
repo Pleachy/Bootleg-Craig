77d64f15b318519bcf0da81bdff40a25cb588790
Pleachy <daniel.golub@gmail.com>
2021-08-25 21:31:09 -0500

    Removed console logs in FetchPosts.js and added a key to each post

--- src/components/FetchPosts.js
+++ src/components/FetchPosts.js
@@ -7,26 +7,21 @@ const FetchPosts = (props) => {
 
     const fetchPosts = async () => {
         const response = await fetch(`${BASE_URL}/posts`);
-        //console.log("response: ", response)
         const data = await response.json();
-        //console.log("data: ", data);
         setPosts(data.data.posts);
     }
 
     useEffect(() => {
         fetchPosts();
-        //console.log("posts inside useEffect: ", posts);
     }, [])
 
     return <>
         <h3>Bootleg Craig</h3>
         {
-            posts ? posts.map((post, index) => <>
-                 <div>
+            posts ? posts.map((post, index) => <div key={post._id}>
                      <h2>{post.title}</h2>
                      <div>{post.description}</div>
-                     </div>
-            </>) : null
+            </div>) : null
         }
     </>
 }
