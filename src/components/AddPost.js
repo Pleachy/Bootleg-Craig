bae848a50c43d08c85dce860475867e0e51428ca
Pleachy <daniel.golub@gmail.com>
2021-08-27 21:28:18 -0500

    Created a form for AddPost, testing out the input checkbox type

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -5,12 +5,36 @@ const AddPost = (props) => {
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
     const [location, setLocation] = useState('');
+    const [willDeliverCheckbox, setWillDeliverCheckbox] = useState('');
     const [willDeliver, setWillDeliver] = useState(false);
 
     return <div>
-        <form>
-            <label></label>
-
+        <form className="new-post">
+            <div>
+                <label>Enter Post Title</label>
+                <input type='text' placeholder='title' value={title}
+                onChange={(event) => setTitle(event.target.value)}></input>
+            </div>
+            <div>
+                <label>Enter Post Description</label>
+                <input type='text' placeholder='description' value={description}
+                onChange={(event) => setDescription(event.target.value)}></input>
+            </div>
+            <div>
+                <label>Enter Item Price</label>
+                <input type='text' placeholder='price' value={price}
+                onChange={(event) => setPrice(event.target.value)}></input>
+            </div>
+            <div>
+                <label>Enter Location</label>
+                <input type='text' placeholder='location' value={location}
+                onChange={(event) => setLocation(event.target.value)}></input>
+            </div>
+            <div>
+                <label>Available For Delivery</label>
+                <input type='checkbox' name='willdeliver' value='true'
+                onChange={(event) => setWillDeliverCheckbox(event.target.value)}></input>
+            </div>
         </form>
         
     </div>
